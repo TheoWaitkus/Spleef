b7e2bdd6f117af44da0379a186449238003e5988
Snaxolas <Pokemonnik@gmail.com>
2020-05-16 18:36:08 -0700

    fixed countdowns (again)

--- src/com/nutter/spleef/CountdownEvent.java
+++ src/com/nutter/spleef/CountdownEvent.java
@@ -2,6 +2,7 @@ package com.nutter.spleef;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class CountdownEvent extends BukkitRunnable {
@@ -10,21 +11,34 @@ public class CountdownEvent extends BukkitRunnable {
     private Main plugin;
     private SpleefGame game;
     private int countdown;
+    private boolean showEveryone;
 
-    public CountdownEvent(Main main, SpleefGame caller, int toCountdown){
+    public CountdownEvent(Main main, SpleefGame caller, int toCountdown, boolean showEveryone){
         plugin = main;
         game = caller;
         countdown = toCountdown;
+        this.showEveryone = showEveryone;
+
     }
     @Override
-    public void run() {
-        if(countdown <= 0){
-            Bukkit.broadcastMessage(ChatColor.DARK_GREEN + Integer.toString(countdown) + "!");
+    public void run()
+    {
+        if(countdown <= 0)
+        {
             this.cancel();
         }
         else
         {
-            Bukkit.broadcastMessage(ChatColor.DARK_GREEN + Integer.toString(countdown) + "!");
+            if(showEveryone)
+            {
+                Bukkit.broadcastMessage(ChatColor.DARK_GREEN + Integer.toString(countdown));
+            }
+            else
+            {
+                for(Player p: game.joinedList){
+                    p.sendMessage(ChatColor.DARK_GREEN + Integer.toString(countdown));
+                }
+            }
         }
         countdown--;
     }
