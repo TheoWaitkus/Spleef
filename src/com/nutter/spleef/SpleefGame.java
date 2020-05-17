25d42f250f2b67926c97d60b290ece1d94446fc5
theotheo36 <theotheo36@gmail.com>
2020-05-16 18:07:14 -0700

    made it only broadcast start of game once

--- src/com/nutter/spleef/SpleefGame.java
+++ src/com/nutter/spleef/SpleefGame.java
@@ -102,11 +102,11 @@ public class SpleefGame
 				world.getBlockAt(x,y,z).setType(Material.SNOW_BLOCK);
 			}
 		}
-
+		Bukkit.broadcastMessage(ChatColor.GOLD + "Join time has ended. Spleefing starting in: " + plugin.getConfig().getString("countdown-time"));
 		//runs for each player registered for the game
 		for(Player p : joinedList)
 		{
-			Bukkit.broadcastMessage(ChatColor.GOLD + "Join time has ended. Spleefing starting in: " + plugin.getConfig().getString("countdown-time"));
+			
 			//writes each player's inventory to a file.
 			ObjectWriter.writeInventory(plugin, p);
 			ObjectWriter.writeCoords(plugin,p);
