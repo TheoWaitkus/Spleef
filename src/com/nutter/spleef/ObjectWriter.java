44f066a8ea9b875f864d52e9680725b5a3452c28
theotheo36 <theotheo36@gmail.com>
2020-05-31 18:36:00 -0700

    checks if the player is online before restoring inventory

--- src/com/nutter/spleef/ObjectWriter.java
+++ src/com/nutter/spleef/ObjectWriter.java
@@ -38,39 +38,40 @@ public class ObjectWriter
 	public static void restoreInventory(Main plugin,Player p)
 	{
         try 
-        { 
-			p.getInventory().clear();
-            FileInputStream fileIn = new FileInputStream(plugin.getDataFolder() + File.separator + "PlayerInventoryData" + File.separator + p.getUniqueId()+".txt");
-            BukkitObjectInputStream objectIn = new BukkitObjectInputStream(new ObjectInputStream(fileIn));
-            Object obj = objectIn.readObject();
-            objectIn.close();
-            
-            SerializableInventory inv = (SerializableInventory) obj;
-
-            //both is a list of both main inventory and armor from the player
-            List<List<Map<String,Object>>> both = inv.getInventory();
-
-            for(int i=0; i< both.get(0).size();i++)//map: inv.getInventory()
-            {
-            	p.getInventory().setItem(i,ItemStack.deserialize(both.get(0).get(i)));
-            }
-
-            //assigning armorContents to specifically the armor portion of both
-            List<Map<String,Object>> armorContents = both.get(1);
-
-            //making it into an arraylist, because that is what setArmorContents takes.
-            ItemStack[] toArmor = new ItemStack[armorContents.size()];
-            
-            for(int i=0; i<armorContents.size();i++)
-            {
-                toArmor[i] = ItemStack.deserialize(armorContents.get(i));
-
-            }
-            p.getInventory().setArmorContents(toArmor);
-            File yourFile = new File(plugin.getDataFolder()+ File.separator +"PlayerInventoryData"+ File.separator +p.getUniqueId()+".txt");
-            if(p.getInventory().getArmorContents().equals(toArmor) && p.getInventory().getContents().equals(to))
-            yourFile.delete();
-
+        {
+        	if(p.isOnline())
+        	{ 
+				p.getInventory().clear();
+	            FileInputStream fileIn = new FileInputStream(plugin.getDataFolder() + File.separator + "PlayerInventoryData" + File.separator + p.getUniqueId()+".txt");
+	            BukkitObjectInputStream objectIn = new BukkitObjectInputStream(new ObjectInputStream(fileIn));
+	            Object obj = objectIn.readObject();
+	            objectIn.close();
+	            
+	            SerializableInventory inv = (SerializableInventory) obj;
+	
+	            //both is a list of both main inventory and armor from the player
+	            List<List<Map<String,Object>>> both = inv.getInventory();
+	
+	            for(int i=0; i< both.get(0).size();i++)//map: inv.getInventory()
+	            {
+	            	p.getInventory().setItem(i,ItemStack.deserialize(both.get(0).get(i)));
+	            }
+	
+	            //assigning armorContents to specifically the armor portion of both
+	            List<Map<String,Object>> armorContents = both.get(1);
+	
+	            //making it into an arraylist, because that is what setArmorContents takes.
+	            ItemStack[] toArmor = new ItemStack[armorContents.size()];
+	            
+	            for(int i=0; i<armorContents.size();i++)
+	            {
+	                toArmor[i] = ItemStack.deserialize(armorContents.get(i));
+	
+	            }
+	            p.getInventory().setArmorContents(toArmor);
+	            File yourFile = new File(plugin.getDataFolder()+ File.separator +"PlayerInventoryData"+ File.separator +p.getUniqueId()+".txt");
+	            yourFile.delete();
+        	}
 
         } 
         catch (Exception ex) 
